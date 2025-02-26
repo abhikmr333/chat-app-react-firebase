09804988c3d3c5dfdbcfbeaaefbf3d22e27c9f87
abhikmr333 <abhi363642@gmail.com>
2025-02-26 18:47:29 +0530

    chore: install dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { appRoute } from "./src/App";
+import "dotenv/config";
 
 const rootNode = document.querySelector("#root");
 const root = createRoot(rootNode);
--- src/components/Users/ChatList.js
+++ src/components/Users/ChatList.js
@@ -1,12 +1,35 @@
 import userAvatar from "/userAvatar.png";
+import { useState, useRef } from "react";
 
 const ChatList = () => {
+    //toggleAddUser is different from addUser
+    const [toggleAddUser, setToggleAddUser] = useState(false);
+    const userRef = useRef("");
+
+    const searchUser = () => {
+        const { value } = userRef.current;
+        console.log("searching for..." + value);
+    };
+
     return (
         <section className="overflow-y-scroll">
             <div className=" p-8 pb-0">
                 <input className="border-1" type="text" placeholder="search" />
-                <button className="bg-blue-500 text-white ml-4"> add </button>
+                <button
+                    onClick={() => setToggleAddUser((prev) => !prev)}
+                    className="bg-blue-500 text-white ml-4"
+                >
+                    add
+                </button>
             </div>
+            {toggleAddUser && (
+                <div className="m-2 p-2 flex justify-between">
+                    <input ref={userRef} className="border-1 rounded-md" type="text" />
+                    <button onClick={searchUser} className="bg-blue-400 text-white">
+                        search
+                    </button>
+                </div>
+            )}
             <div>
                 <div className="flex items-center gap-5 p-5 cursor-pointer border-b-black border-b-1">
                     <img src={userAvatar} className="w-[30px]" alt="User Avatar" />
