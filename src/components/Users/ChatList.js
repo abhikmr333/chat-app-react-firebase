dd7625be2cd426cd6098e4c92b5e8f754440210d
abhikmr333 <abhi363642@gmail.com>
2025-03-29 12:24:52 +0530

    feat(chatlist): changing the isSeen of the userchat to true whenever current-user clicks on it

--- src/components/Users/ChatList.js
+++ src/components/Users/ChatList.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { onSnapshot, doc, getDoc } from "firebase/firestore";
+import { onSnapshot, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../lib/firebase";
 import AddUser from "./AddUser";
 import { useSelector, useDispatch } from "react-redux";
 import { changeChatView } from "../../redux/features/chatSlice";
+import { findNonSerializableValue } from "@reduxjs/toolkit";
 
 const ChatList = () => {
     const currentUser = useSelector((state) => state.user.currentUser);
@@ -20,7 +21,6 @@ const ChatList = () => {
                 const items = document.data()?.chats;
                 //now using each item.receiverId fetching the user detail to show their name and avatar
                 const promises = items.map(async (item) => {
-                    console.log(item);
                     const usersDocRef = doc(db, "users", item.receiverId);
                     const usersDocSnap = await getDoc(usersDocRef);
                     const userData = usersDocSnap.exists() ? usersDocSnap.data() : null;
@@ -36,8 +36,27 @@ const ChatList = () => {
         }
     }, [currentUser?.id]);
 
-    const handleSelectedChat = (chat) => {
-        dispatch(changeChatView({ ...chat, currentUser }));
+    const handleSelectedChat = async (chat) => {
+        //changing isSeen when the currentUser clicks on the new message
+        // separating userData and userChatData (to not retrieve userChats associated with currentUser again since we already did once)
+        //here chats and chat is different
+        const userChats = chats.map((chat) => {
+            const { userData, ...rest } = chat;
+            return rest;
+        });
+        //find the index at which the user with similar chat.chatId is located inside userChats
+        const chatIndex = userChats.findIndex((userChat) => userChat.chatId === chat.chatId);
+        userChats[chatIndex].isSeen = true;
+        // do changes in that index and replace the old userChats with updated one in firebase using updateDoc
+        const userChatsRef = doc(db, "userchats", currentUser.id);
+        try {
+            await updateDoc(userChatsRef, {
+                chats: userChats,
+            });
+            dispatch(changeChatView({ ...chat, currentUser }));
+        } catch (err) {
+            console.log(err.message);
+        }
     };
 
     return (
